caee7d57d5025885165f92ff06962aa7aede04f5
Chen <xiaokunchen@outlook.com>
2024-03-17 20:52:05 -0400

    feat: define the models

---
+++ models/item.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    name: {type: String, required: true, maxLEngth: 120},
+    description: {type: String, required: false, maxLength: 120},
+    price: {type: Number, required: true},
+    num_in_stock: {type: Number, required: true},
+    category: {type: Schema.Types.ObjectId, ref: 'Category', required: true}
+});
+
+// Virtual for item's URL
+ItemSchema.virtual('url').get(function () {
+    return '/item/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Item', ItemSchema);
