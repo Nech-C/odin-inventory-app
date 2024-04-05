af9c060832ef049111749b7fb7a4cc741e2d1be6
Chen <xiaokunchen@outlook.com>
2024-04-05 17:39:49 -0400

    feat: impl item update func

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -98,12 +98,80 @@ exports.item_create_post = [
 ]
 
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item update GET");
+    const item = await Item.findById(req.params.id).exec();
+    const categories = await Category.find().exec();
+    if (item === null) {
+        const err = new Error("Item not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("item_form", {
+        title: "Update Item",
+        item: item,
+        categories: categories,
+    });
 });
 
-exports.item_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item update POST");
-});
+exports.item_update_post = [
+    body("name")
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Name must be specified.")
+        .isAlphanumeric()
+        .withMessage("Name has non-alphanumeric characters."),
+    body("description")
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Description must be specified.")
+        .isAlphanumeric()
+        .withMessage("Description has non-alphanumeric characters."),
+    body("category")
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Category must be specified.")
+        .isAlphanumeric()
+        .withMessage("Category has non-alphanumeric characters."),
+    body("price")
+        .isNumeric()
+        .escape()
+        .withMessage("Price must be a number.")
+        .custom((value) => value > 0)
+        .withMessage("Price must be greater than 0."),
+    body("stock")
+        .isNumeric()
+        .escape()
+        .withMessage("Stock must be a number.")
+        .custom((value) => value >= 0)
+        .withMessage("Stock must be greater than or equal to 0."),
+    asyncHandler(async (req, res, next) => {
+        errors = validationResult(req);
+        const categories = await Category.find().exec();
+        const item = new Item({
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            price: req.body.price,
+            num_in_stock: req.body.stock,
+            _id: req.params.id,
+        });
+
+        if (!errors.isEmpty()) {
+            res.render("item_form", {
+                title: "Update Item",
+                item: item,
+                categories: categories,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            const updatedItem = await Item.findByIdAndUpdate(req.params.id, item, {new: true});
+            res.redirect(updatedItem.url);
+        }
+    })
+]
 
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED: Item delete GET");
