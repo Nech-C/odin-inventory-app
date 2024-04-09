a42e957b96f77cef0db588a77e0dd0d7cfef79a6
Chen <xiaokunchen@outlook.com>
2024-04-09 14:04:49 -0400

    feat: impl add category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -8,7 +8,41 @@ exports.category_create_get = asyncHandler(async (req, res, next) => {
     res.render("category_form", {title: "Create Category"});
 });
 
-exports.category_create_post = [];
+exports.category_create_post = [
+    body("category_name")
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Category name must be specified.")
+        .isAlphanumeric()
+        .withMessage("Category name has non-alphanumeric characters."),
+    body("category_description")
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Category description must be specified.")
+        .isAlphanumeric()
+        .withMessage("Category description has non-alphanumeric characters."),
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.render("category_form", {
+                title: "Create Category",
+                category_name: req.body.category_name,
+                category_description: req.body.category_description,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            const category = new Category({
+                name: req.body.category_name,
+                description: req.body.category_description,
+            });
+            await category.save();
+            res.redirect(category.url);
+        }
+    })
+];
 
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED: Category delete GET");
